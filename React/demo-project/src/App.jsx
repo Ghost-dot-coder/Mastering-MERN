5888e0cb3abe7e1437012761f07922be9fb19a0d
Manu <manukurisumkal@gmail.com>
2025-08-22 10:48:59 +0530

    end of js

---
+++ React/demo-project/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const empNAme = "rahul"
+  return (
+    <>
+      <h1 className="text-center text-danger">Demo project</h1>
+      <h3 >Inside app component: <span className="text-primary fw-bolder">{empNAme}</span></h3>
+    </>
+  );
+}
+
+export default App;
