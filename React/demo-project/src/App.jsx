ae3066b8da22c61330f85dace4779708335819fd
Manu <manukurisumkal@gmail.com>
2025-08-23 15:14:50 +0530

    updated

--- React/demo-project/src/App.jsx
+++ React/demo-project/src/App.jsx
@@ -3,13 +3,18 @@ import Employee from "./Employee";
 
 function App() {
   const empNAme = "rahul";
+  const ShowAlert = () =>{
+    alert("Button clicked")
+  }
   return (
     <>
       <h1 style={{textAlign:"center", color:"red"}}>Demo project</h1>
       <Employee dataFromParent={empNAme} empDesignation="Deigner" empStaus />
       <Company />
-
-
+    <div className="text-center">
+      <button className="btn btn-primary" onClick={ShowAlert}>Show alert</button>
+  
+    </div>
     </>
   );
 }
