b68129b480836db5a85cefd138e3622671f5cc3c
Manu <manukurisumkal@gmail.com>
2025-08-25 21:21:40 +0530

    updated

--- React/demo-project/src/App.jsx
+++ React/demo-project/src/App.jsx
@@ -1,20 +1,36 @@
 import Company from "./Company";
 import Employee from "./Employee";
+import MyCar from "./MyCar";
 
 function App() {
   const empNAme = "rahul";
-  const ShowAlert = () =>{
-    alert("Button clicked")
+  const ShowAlert = () => {
+    alert("Button clicked");
+  };
+  const displayData= (data)=>{
+    alert(`The data is: ${data}`)
+  }
+  const getUserData = (e)=>{
+    console.log(e);
+    
   }
   return (
     <>
-      <h1 style={{textAlign:"center", color:"red"}}>Demo project</h1>
+      <h1 style={{ textAlign: "center", color: "red" }}>Demo project</h1>
       <Employee dataFromParent={empNAme} empDesignation="Deigner" empStaus />
       <Company />
-    <div className="text-center">
-      <button className="btn btn-primary" onClick={ShowAlert}>Show alert</button>
-  
-    </div>
+      <div className="text-center">
+        <button className="btn btn-primary" onClick={ShowAlert}>
+          Show alert
+        </button><br />
+        <button className="btn btn-success mt-4" onClick={()=>displayData(empNAme)}>
+          Show Data
+        </button>
+        <div className="w-50 m-5">
+          <input className="form-control" type="text" onChange={(e)=>getUserData(e.target.value)} placeholder="Enter Name" />
+        </div>
+      </div>
+      <MyCar/>
     </>
   );
 }
---
+++ React/demo-project/src/MyCar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+// rcc - class based component
+export default class MyCar extends Component {
+  render() {
+    return (
+      <div style={{minHeight:"50vh"}} className='p-5 bg-warning rounded'>MyCar</div>
+    )
+  }
+}
