a4ad7eb7ee9ab6e3c6657ae50247d76252a0bb4c
Manu <manukurisumkal@gmail.com>
2025-10-10 10:15:26 +0530

    Bookstore v2

--- React/bookstore/frontend/src/App.jsx
+++ React/bookstore/frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+    <h1 className="text-4xl text-amber-300 text-center">bookstore</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
